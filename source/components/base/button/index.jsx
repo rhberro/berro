d4afe608b0e1b05d3c193dc6879757ae82f9bf99
Rafael Henrique Berro <rhberro@gmail.com>
2019-07-14 01:33:55 -0300

    testing the lint-staged library

--- source/components/base/button/index.jsx
+++ source/components/base/button/index.jsx
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-function Button({ children, onClick }) {
+function Button(
+  { children, onClick }
+) {
   return <button onClick={onClick}>{children}</button>
 }
 
