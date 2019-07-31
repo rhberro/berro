2b35306eae54e5df23bf8e0a6c5ccbf504811485
Rafael Henrique Berro <rhberro@gmail.com>
2019-07-31 00:01:05 -0300

    fixing the environment variables

--- source/constants/environment.js
+++ source/constants/environment.js
@@ -1,14 +1,14 @@
 /**
  * The users service host.
  */
-export const SERVICE_USERS_HOST = `${process.env.SERVICE_USERS_URL}:${process.env.SERVICE_USERS_PORT}`
+export const SERVICE_USERS_HOST = process.env.SERVICE_USERS_URL
 
 /**
  * The tasks service host.
  */
-export const SERVICE_TASKS_HOST = `${process.env.SERVICE_TASKS_URL}:${process.env.SERVICE_TASKS_PORT}`
+export const SERVICE_TASKS_HOST = process.env.SERVICE_TASKS_URL
 
 /**
  * The cases service host.
  */
-export const SERVICE_CASES_HOST = `${process.env.SERVICE_CASES_URL}:${process.env.SERVICE_CASES_PORT}`
+export const SERVICE_CASES_HOST = process.env.SERVICE_CASES_URL
